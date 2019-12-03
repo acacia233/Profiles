531e2e4d34b3be1a4f9ac101a7841e85b7131068
acacia233 <55625991+acacia233@users.noreply.github.com>
2019-12-03 13:22:57 +0800

    Create ticktick.js

---
+++ Surge/Js/ticktick.js
@@ -0,0 +1,12 @@
+let body = $response.body
+body=JSON.parse(body)
+body['pro']=true
+body['proEndDate']="2099-10-10T03:27:34.000+0000"
+body=JSON.stringify(body)
+$done({body}) 
+
+/*
+滴答清单本地解锁VIP
+频道：https://t.me/acacia233
+MITM ticktick.com dida365.com
+*/
