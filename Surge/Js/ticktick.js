47dc742e28967d04db8204aec54b6837c24e7506
acacia233 <55625991+acacia233@users.noreply.github.com>
2020-02-14 16:05:47 +0800

    Merge

--- Surge/Js/ticktick.js
+++ Surge/Js/ticktick.js
@@ -3,11 +3,12 @@ body=JSON.parse(body)
 body['pro']=true
 body['proEndDate']="2050-05-05T05:55:55.000+0000"
 body=JSON.stringify(body)
-$done({body}) 
+$done({body})
 
 /*
 滴答清单本地解锁VIP
 频道：https://t.me/acacia233
+仅限老版本可用 新版本已经无法MITM
 MITM ticktick.com dida365.com
 http-response ^https?:\/\/(dida365|ticktick)\.com\/api\/v2\/user\/status requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/acacia233/SimpleRule/master/Surge/Js/ticktick.js,script-update-interval=-1
 */
